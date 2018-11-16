0338aa923ed4b78eed8463034c556dee4cdbe25b
Edwin Lin <edwinlin@gmail.com>
2018-11-16 17:08:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 function dwarfRollCall(dwarves) {
+  var string = ""
+  var i = 0
+  while (i < dwarves.length){
+    string += `${i+1}. ${dwarves[i]} `
+    i++
+  }
+  return string
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var array = []
+  var i = 0
+  while(i < planeteerCalls.length){
+    array.push(planeteerCalls[i].toUpperCase() + "!")
+    i++
+  }
+  return array
 }
 
 function longPlaneteerCalls(words) {
+  var i = 0
+  while(i < words.length){
+    if (words[i].length > 4){
+      return true
+    }
+    i++
+  }
+  return false
 }
 
 function findTheCheese (foods) {
+  var cheeses = ['cheddar', 'gouda', 'camembert']
+  var i = 0
+  while(i < foods.length){
+    var j = 0
+    while(j<cheeses.length){
+      if(foods[i] === cheeses[j]){
+        return foods[i]
+      }
+      j++
+    }
+    i++
+  }
+  
+  // foods.forEach(function(food){
+  //   cheeses.forEach(function(cheese){
+  //     if (food === cheese){
+  //       return food
+  //     }
+  //   })
+  // })
+
+  return 'no cheese!'
 }
