99f4508cb312b1ee055256405430b7e953d65fb8
Edwin Lin <edwinlin@gmail.com>
2018-11-16 18:58:59 +0000

    Done.

--- index.js
+++ index.js
@@ -31,25 +31,40 @@ function longPlaneteerCalls(words) {
 
 function findTheCheese (foods) {
   var cheeses = ['cheddar', 'gouda', 'camembert']
-  var i = 0
-  while(i < foods.length){
-    var j = 0
-    while(j<cheeses.length){
-      if(foods[i] === cheeses[j]){
-        return foods[i]
-      }
-      j++
-    }
-    i++
-  }
   
+  // var i = 0
+  // while(i < foods.length){
+  //   var j = 0
+  //   while(j<cheeses.length){
+  //     if(foods[i] === cheeses[j]){
+  //       return foods[i]
+  //     }
+  //     j++
+  //   }
+  //   i++
+  // }
+  
+  // var array = []
   // foods.forEach(function(food){
   //   cheeses.forEach(function(cheese){
+  //     // console.log(food + " " + cheese)
+  //     // console.log(food === cheese)
   //     if(food === cheese){
-  //       return food
+  //       array.push(food)
   //     }
   //   })
   // })
+  // if (array.length > 0){
+  //   return array[0]
+  // }
+  // forEach method does not break during loop
 
+  // var i = 0
+  // while(i<foods.length){
+  //   if (cheeses.includes(foods[i])){
+  //     return foods[i]
+  //   }
+  //   i++
+  // }
   return 'no cheese!'
 }
